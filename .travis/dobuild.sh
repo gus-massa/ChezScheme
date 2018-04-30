837a41bb2884a46f9000a922cf2ee4f09a838ccf
Gustavo Massaccesi <gustavo@oma.org.ar>
2018-04-30 18:32:41 -0300

    run test

--- .travis/dobuild.sh
+++ .travis/dobuild.sh
@@ -14,6 +14,9 @@ if [ $exitcode -ne 0 ] ; then
   echo "Failed: make step"
   exit $exitcode
 fi
+echo !!!!!!
+( export SCHEMEHEAPDIRS=.:../../boot/${TARGET_MACHINE} && cd ${TARGET_MACHINE}/bin/${TARGET_MACHINE} && ./scheme -q < ../../../test.ss )
+echo !!!!!!
 ( cd ${TARGET_MACHINE}/mats && make partialx 2>&1 ) | tee Make.out | grep '^matting '
 diff -q .travis/summary ${TARGET_MACHINE}/mats/summary
 exitcode=$?
