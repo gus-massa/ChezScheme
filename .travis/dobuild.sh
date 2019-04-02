bf68766d195c5b7ea4d8f64469f8c990bb7ef67a
Gustavo Massaccesi <gustavo@oma.org.ar>
2019-04-02 17:34:06 -0300

    rebuild in travis the bootfiles of the non thread version

--- .travis/dobuild.sh
+++ .travis/dobuild.sh
@@ -3,6 +3,27 @@ case $TARGET_MACHINE in
   *i3le) sudo apt-get -yq --no-install-suggests --no-install-recommends install uuid-dev:i386 ;;
   *)
 esac
+
+case $TARGET_MACHINE in
+  *i3osx) PRE_TARGET_MACHINE=ti3osx;;
+  *a6osx) PRE_TARGET_MACHINE=ta6osx;;
+  *i3le) PRE_TARGET_MACHINE=ti3le;;
+  *a6le) PRE_TARGET_MACHINE=ta6le;;
+  *i3nt) PRE_TARGET_MACHINE=ti3nt;;
+  *a6le) PRE_TARGET_MACHINE=ta6nt;;
+  *)
+esac
+if [ $PRE_TARGET_MACHINE ] ; then
+  ./configure -m=$PRE_TARGET_MACHINE
+  make
+  make bootfiles bootfiles=${TARGET_MACHINE}.boot
+  exitcode=$?
+  if [ $exitcode -ne 0 ] ; then
+    echo "Failed: bootfile step"
+    exit $exitcode
+  fi
+fi
+
 ./configure -m=$TARGET_MACHINE
 exitcode=$?
 if [ $exitcode -ne 0 ] ; then
@@ -15,10 +36,10 @@ if [ $exitcode -ne 0 ] ; then
   echo "Failed: make step"
   exit $exitcode
 fi
+
 ( cd ${TARGET_MACHINE}/mats && make partialx 2>&1 ) | tee Make.out | grep '^matting '
 diff -q .travis/summary ${TARGET_MACHINE}/mats/summary
 exitcode=$?
-
 if [ $exitcode -ne 0 ] ; then
   echo "Failed: testing step"
   echo "mats summary:"
